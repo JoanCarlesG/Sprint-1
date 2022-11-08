1a1260f1863ff9216a4490d934ee8678f1d50a7e
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-08 12:59:47 +0100

    Exercici 5

--- Tema-3/nivell-1/main.php
+++ Tema-3/nivell-1/main.php
@@ -1,11 +1,12 @@
 <?php 
-$X = 2;
-$Y = 3;
-$N = 1.5;
-$M = 2.5;
-echo "$X <br> $Y <br> Suma ",$X + $Y, "<br> Resta ",$X - $Y, "<br> Producte ",$X * $Y, "<br> Mòdul ",$X % $Y;
-echo "<br>$N <br> $M <br> Suma ",$N + $M, "<br> Resta ",$N - $M, "<br> Producte ",$N * $M, "<br> Mòdul ",$N % $M;
-echo "<br> Doble de variable X: ", $X * 2,"<br> Doble de variable Y: ", $Y * 2,"<br> Doble de variable N: ", $N * 2,"<br> Doble de variable M: ", $M * 2;
-echo "<br> Suma total: ", $X + $Y + $N + $M;
-echo "<br> Producte total: ", $X * $Y * $N * $M;
+$nums1 = array(5, 6, 3, 4, 2);
+$nums2 = array(7, 8, 1);
+echo "<pre>",var_dump($nums1);
+echo "<pre>",var_dump($nums2);
+
+$nums2[] = 9;
+echo "<pre>",var_dump($nums2);
+
+$merged = array_merge($nums1, $nums2);
+echo "<pre>",var_dump($merged);
 ?>
