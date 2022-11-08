4c3046dd6c73b0da0014c491a0e9d7762caf13bf
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-08 12:41:22 +0100

    Exercici 4

--- Tema-3/nivell-1/main.php
+++ Tema-3/nivell-1/main.php
@@ -1,4 +1,11 @@
 <?php 
-define("name", "<h1><strong>Joan Carles</strong></h1>");
-echo name;
+$X = 2;
+$Y = 3;
+$N = 1.5;
+$M = 2.5;
+echo "$X <br> $Y <br> Suma ",$X + $Y, "<br> Resta ",$X - $Y, "<br> Producte ",$X * $Y, "<br> Mòdul ",$X % $Y;
+echo "<br>$N <br> $M <br> Suma ",$N + $M, "<br> Resta ",$N - $M, "<br> Producte ",$N * $M, "<br> Mòdul ",$N % $M;
+echo "<br> Doble de variable X: ", $X * 2,"<br> Doble de variable Y: ", $Y * 2,"<br> Doble de variable N: ", $N * 2,"<br> Doble de variable M: ", $M * 2;
+echo "<br> Suma total: ", $X + $Y + $N + $M;
+echo "<br> Producte total: ", $X * $Y * $N * $M;
 ?>
