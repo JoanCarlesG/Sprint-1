c831c249f3d4e2e81d17c77b0dc8806bccd7e881
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-09 10:28:57 +0100

    Exercici 4

--- Tema-3.2/nivell-1/main.php
+++ Tema-3.2/nivell-1/main.php
@@ -1,16 +1,12 @@
 <?php 
 
-$limit = 23;
-function countTo($limit) {
+function countTo($limit = 10) {
     $start = 0;
     while ($start <= $limit){
         echo $start,"<br>";
         $start += 2;
     }
-    if($limit % 2 != 0){
-        echo $limit;
-    }
 }
 
-echo countTo($limit);
+echo countTo();
 ?>
