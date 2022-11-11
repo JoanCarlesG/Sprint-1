8e234b8af9760ed3a28db7da4e1da4b624139120
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-11 18:59:33 +0100

    Exercici 3

--- Tema-3.2/nivell-2/main.php
+++ Tema-3.2/nivell-2/main.php
@@ -40,6 +40,34 @@
         }
         echo pay();
     ?>
+    Exercici 3
+    <form action="main.php" method="get">
+        Xocolata (1€): <input type="number" name="xoco">
+        <br>
+        Xiclets (0,5€): <input type="number" name="gum">
+        <br>
+        Caramels (1,5€): <input type="number" name="candy">
+        <br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    <?php
+        function costXoco($units){
+            return $units;
+        }
+        function costGum($units){
+            return $units * 0.5;
+        }
+        function costCandy($units){
+            return $units * 1.5;
+        }
+        
+        echo "Xocolata: ", $_GET["xoco"], "<br>";
+        echo "Xiclets: ", $_GET["gum"], "<br>";
+        echo "Caramels: ", $_GET["candy"], "<br>";
+        echo "Total: ";
+        echo costXoco($_GET["xoco"]) + costGum($_GET["gum"]) + costCandy($_GET["candy"]);
+        echo "€";
+    ?>
 </body>
 
 </html>
