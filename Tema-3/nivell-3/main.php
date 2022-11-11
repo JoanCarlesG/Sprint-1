e78b65dfbdf93e2a11b049aa4da551c225a4e201
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-11 17:10:23 +0100

    Exercici 1

--- Tema-3/nivell-3/main.php
+++ Tema-3/nivell-3/main.php
@@ -9,17 +9,22 @@
 
 <body>
     Exercici 1
-    <form action="example.php" method="get">
+    <form action="main.php" method="get">
         Input Text: <input type="text" name="txt">
         <br>
-        <input type="submit" name="submit" value="submit">
+        <input type="submit" name="submit" value="Submit">
         <br>
     </form>
     <p>The answer is:</p>
     <?php
-        $txt = $_GET["txt"];
-        $txtarray = str_split($txt);
-        echo $txtarray[count($txtarray) - 1].substr($txt, 1, -1).$txtarray[0];
+        if(isset($_GET["submit"])){
+            $txt = $_GET["txt"];
+            $txtarray = str_split($txt);
+            $trimmedarray = array_filter(array_map("trim",$txtarray));
+            echo "<pre>"; 
+            echo var_dump($trimmedarray);
+            echo "</pre>";
+        }
     ?>
 </body>
 
